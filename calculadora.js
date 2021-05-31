802285cb33f654b0f8552123ee1f24cad71a9d28
PabloAristizabal <69635694+PabloAristizabal@users.noreply.github.com>
2021-05-30 23:18:30 -0500

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+let number_one; 
+
+function dato_one(){
+    document.getElementById("input_data").innerText =document.getElementById("input_data").textContent + "1";
+}
+function dato_two(){
+    document.getElementById("input_data").innerText =document.getElementById("input_data").textContent + "2";
+}
